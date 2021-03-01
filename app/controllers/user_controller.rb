b558afe2d128137e19456b6ea91a401a53ac7c20
Kevin <kevhines@gmail.com>
2021-03-01 17:31:36 -0500

    updating and displaying base user info

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -39,7 +39,23 @@ class UserController < ApplicationController
         else
             redirect '/login'
         end
-    
     end
+
+    get '/user/:id' do
+        @user = User.find_by(id: params[:id])
+        erb :'/user/show'
+    end
+
+    get '/user/:id/edit' do
+        @user = User.find_by(id: params[:id])
+        erb :'/user/edit'
+    end
+
+    patch '/user/:id' do
+        @user = User.find_by(id: params[:id])
+        @user.update(params[:user])
+        redirect "/user/#{@user.id}"
+    end
+
 
 end
