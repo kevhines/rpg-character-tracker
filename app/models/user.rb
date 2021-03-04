e50ff7a04290a35668913b50b065a193ed71b203
Kevin <kevhines@gmail.com>
2021-03-04 16:30:52 -0500

    alters validation to not allow same usernames with different cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
     has_many :notes
     has_many :goods
     has_secure_password
-    validates :username, presence: true, format: { with: /\A\w+\z/, message: "doesn't allow spaces or special characters" }, uniqueness: true
+    validates :username, presence: true, format: { with: /\A\w+\z/, message: "doesn't allow spaces or special characters" }, uniqueness: { case_sensitive: false }
 end
