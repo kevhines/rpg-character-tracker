f6eecea9bec5886d4675713338fdd786986c81b1
Kevin <kevhines@gmail.com>
2021-03-05 10:33:07 -0500

    adds seed data

---
+++ db/seeds.rb
@@ -0,0 +1,77 @@
+syracuse = User.create(username: "Syracuse", password: "123")
+michigan = User.create(username: "Michigan", password: "123")
+texas = User.create(username: "Texas", password: "123")
+seattle = User.create(username: "Seattle", password: "123")
+nyc = User.create(username: "NYC", password: "123")
+
+syracuse.character_name = "Merritt"
+syracuse.bio = "Blink. The boy was young. Really young. Running though the streets of the city. An urchin in a pack of urchins. Dirty. Torn. Stained. Abandoned by a human mother, shamed by her family for having lain with an elf. And unwanted by an entire race who considered him...less. Here, on the streets, all that mattered were his wits, his cunning and his boyish charm. The tools of his panhandling trade. The shape of his ears meant little to the other children he ran with. Blink. Older now. More daring. In this instance, too daring. Caught by the wizened old man whose home he was trying to rob. The man a little more spry, and a little more at home, than the boy expected. Caught up in unseen ropes, the wizard had easily subdued him. But something in the boyʼs eyes, a rich deep brown, flecked with gold, told the wizard not only a story of a hungry belly, but of intelligence and determination.Blink. Older once more. This time taking care and effort to polish the yew quarterstaff he had painstakingly carved. It would be a focus for his spell casting, if his master ever let him even try real magic. 'Patience, lad. Nothing ever came from rushing the work. In due time. Now, finish what youʼve got there, before your lesson in the yard. Iʼm not so old I donʼt remember my youth as well. Need to get out in that sunshine. Flex those muscles. Swing a staff or sword or two. Canʼt stay holed up in this library all day. Not a boy of your age.'Blink. Closer now. A history. Of the war. Of artifacts fought with, over and for. Thought lost. And an idea, in a young mind full of wanderlust. Grab your pack. Grab your staff. Ferret your gear out in the middle of the night and take to the road. And then off the road, wandering old trails, and when those trails disappeared, forging your own path ahead. Resolute. Determined. Until you wind up here. Encased in something not entirely understood. 'Wit. Cunning. Intelligence. Charisma. Determination. Boldness. I can use these things...I can use you. You have... merit. What do you say, boy? Serve my will in this world and I will provide you with the things you most desire...power, magic, knowledge. You will travel this world and others, serving my will, but your own being. Your own self. You will be more.' Images assailed the young manʼs mind of battles fought, magics cast, and powers wielded. Of traveling the farthest reaches of the land, and accumulating artifacts of power throughout. Of learning the secrets of the multiverse, with a veritable god as his guide. 'Yes.', the young man spoke.  Not with words, but from his heart, with his soul. The deal struck. 'Done then.', the voice replied. 'I name you Merritt. For your worthiness. Receive my reward.'With those words, the metallic weave encircling Merritt begins to twist and writhe. Those intricately woven bands begin to form runes older than time, the metal flares molten hot and Merritt screams as his patronʼs will is written into his very bones. The metal burns its way under his skin. The runes transforming his essence. Reassembling what was written. It lasts only minutes, that feel like centuries, and when it is done, faint scars are all that remain. Rune scars tattooed over his entire body. Only his face and his hands remain untouched. 'Go out into this world, and test your new found powers. Let the thirst for knowledge be your guide. Together we will see what this world has to offer. Know that I will be with you...always.'"
+syracuse.description = "Stoic and weaponless. Dresses in dark clothes. Half-elf."
+syracuse.save
+michigan.character_name = "Marcus Dawntracker"
+michigan.bio = "finding himself alone in the woods, yet again, is at home.  The wars raged when he was entering his prime.  Navigating times of war honed his skills, and reinforced his hatred for those that do not live by at least some principles.  Following the wars, he found himself wandering the woods and countryside, doing his part to 'cleanse' the area of scum hiding in its depths.  A small but great comfort, that still comes today, through all of these travails, is capturing a piece of his adventures in mini-totems.  Woodcarving still brings peace.  The fire though, to 'cleanse the forests' still burns.  It is hard for a half-elf battlemaster to simply sit in the forest and wait for something interesting to happen.  The whistle of the bowstring is a sweet sound, especially when they don't even know it is coming."
+michigan.description = "Half-elf. Armed with an elven bow and a quiver of arrows. Jovial, but quick to anger."
+michigan.save
+texas.character_name = "Matthias"
+texas.bio = "He had been born without any last name at all.  Just 'Matthias', a simple boy who learned to keep his head down in those dark times before the Baron came.  But when the Baron did come and fought to set things right – or at least less wrong – Matthias was one of the first who stood up to help fight.  He was young and not really brave – just didn’t understand the danger enough to be as scared as he should have been.  When the Spider Queen came, he did his part.   He and the other rangers held their post. Long enough to keep the flanks safe.  Long enough to make a difference.  They had paid a heavy price though.  Once the enemy has retreat, he had rushed to join the main combat.  As it happened, Matthias’ was the first face Baron McGown saw after his brazen leap from castle Ravenloft to the battlefield below.  It must have made an impression, though. While Matthias never thought he really deserved it, he was one of a handful that the Baron elevated into the fledgling nobility after the Barony was officially established.  The growing nation needed the machinery of government to operate smoothly, and veterans of the war made up the majority of this new noble class.  Deserved or not, he was happy for the chance to do his part for his homeland – and very happy he would set out into the woods in that service one more time"
+texas.description = "Grey haired, though a bit of red remains in his beard. He dresses a bit too nice to be walking around in the woods with this crew."
+texas.save
+seattle.character_name = "Frederick"
+seattle.bio = "Bang! Ring! Bang! Ring! Bang! Ring! Bang! Ring! Psst! What was at best an annoyance to others were the bells of Arthur's church. They rang true that day as Arthur was one with his faith. While he did not divine the drastic change that was about to befell him he was always ready and eager to leave Krezg. Bang! Ring! Bang! Ring! Bang! Ring! --- Arthur looked up from his spiritual work as his adoptive father entered in haste and gathered a few things. 'Look boy, I'll be back in a few days', commanded the middle aged man. 'Keep about your chores and see to the front of house.' With that he hustled out and joined the men outside. It would be a short travel back to the lord's keep, but not a quick trip. Psst! Arthur put down the sword-in-making and stood contemplative. He knew what this meant. They would soon be back for him and he was done serving tyrants. He knew this day would come and knew what he had to do."
+seattle.description = "Heavily armored. Very uncomfortable with exploring. Would prefer to stand in a forge all day."
+seattle.save
+nyc.character_name = "Owyn Dunne"
+nyc.bio = "Once was a follower of Path, but has since broken off into a small sect called Path's Thunder. He awaits a great storm from the Gods to wipe out most of mankind leaving nature in control once more."
+nyc.description = "Hewas dirty. Maybe homeless? He had a brown cloak draped over his shoulders, and used an oldshepherd's crook to steady himself. His hair was shaggy, his moustache was unkempt, and hischin was covered in dark stubble.. He had a number of small punches tied to his belt, and moretied diagonally along his chest. His left hand held the crook as his right hand smoothed out hismoustache before moving down towards one of the pouches"
+nyc.save
+
+syracuse.goods << Good.create(quantity: "25", name: "Silver Coins")
+syracuse.goods << Good.create(quantity: "1", name: "Amulet, with an image of a demon on the back.", secret: 1)
+syracuse.goods << Good.create(quantity: "1", name: "knapsack")
+syracuse.goods << Good.create(quantity: "3", name: "silver daggers")
+michigan.goods << Good.create(quantity: "1", name: "elven bow")
+michigan.goods << Good.create(quantity: "50", name: "arrows")
+michigan.goods << Good.create(quantity: "25", name: "silver coins")
+michigan.goods << Good.create(quantity: "1", name: "evil dagger")
+texas.goods << Good.create(quantity: "1", name: "Writ from Baron McGowan")
+texas.goods << Good.create(quantity: "1", name: "Rapier")
+texas.goods << Good.create(quantity: "1", name: "Fancy Cloack")
+texas.goods << Good.create(quantity: "1", name: "Orders to Assasinate the Stag Lord", secret: 1)
+texas.goods << Good.create(quantity: "1", name: "locket of his dead wife", secret: 1)
+seattle.goods << Good.create(quantity: "1", name: "Hammer")
+seattle.goods << Good.create(quantity: "1", name: "Holy Symbol")
+seattle.goods << Good.create(quantity: "1", name: "Full Plate")
+seattle.goods << Good.create(quantity: "1", name: "block of silver ore")
+seattle.goods << Good.create(quantity: "1", name: "blood red diamond", secret: 1)
+nyc.goods << Good.create(quantity: "5", name: "Copper coins")
+nyc.goods << Good.create(quantity: "1", name: "bag of deer jerky")
+nyc.goods << Good.create(quantity: "1", name: "amulet of elven maiden", secret: 1)
+nyc.goods << Good.create(quantity: "1", name: "Bastard Sword")
+nyc.goods << Good.create(quantity: "1", name: "holy symbol, selfmade")
+
+syracuse.notes << Note.create(content: "Stopped at Black Falcon Tower")
+syracuse.notes << Note.create(content: "Arrived at Valo Homestead")
+syracuse.notes << Note.create(content: "Killed Bandits/Happs coming to collect 'Protection'")
+michigan.notes << Note.create(content: "Slaughtered Kobolds who ate too much Moon Radish")
+michigan.notes << Note.create(content: "Visited Baskin - Mad Hermit")
+michigan.notes << Note.create(content: "Bandits, led by Kessel, attacked Fort. We are victorious but only by the thinnest of margins")
+texas.notes << Note.create(content: "Attacked by trapdoor spiders. Knocked tricky Grig faerie out and punk'd her. Attacked by evil trees. Mattias and Marcus captured by frogs. ")
+texas.notes << Note.create(content: "Made deal with Laris the Fey to protect the forest in exchange for boon ")
+texas.notes << Note.create(content: "Bold Merrit loots the tomb of Avran Vandir Slaughtered bandit lumberjacks at request of Laris (because they definitely deserved it); slaughtered Bear guardian of Path temple Attacked Mite Lair under the claw tree. ")
+seattle.notes << Note.create(content: "Killed queen, liberated kobold idol; attacked by werewolf. We killed it. attacked by Wyvern and summoned displacer beast. Probably probe from stag lord or lackeys. followed tuskgutter to his barrow, explored. Spent a night there talked to Larys, he told us to go fight stag lord, so we did. Arrived early. Watched for a day, saw stag lord leave, entered at night; Sent a signal to staglord to return (Freed Stag Lords pop) killed stag lord on boats headed to hermit brother")
+seattle.notes << Note.create(content: "Gambled. Left with his fingers. Ran into northrupple saw elf ghost, arrived at trolls, hid for a while.entered troll place got smacked around, ran away back to barrow - took a full day. hunted for wolves, Marcus got bit, raised Mattias")
+seattle.notes << Note.create(content: "Arrived at evil mist lodge from hell. Got invited to Raven Court by Thelamandrin to avoid 'initiation' with the werewolves. Slept by water fall.")
+seattle.notes << Note.create(content: "Went to evil ghost elf tower (fought ghosts and some strange/annoying creature (Skin Tailor?) shooting us with arrows) - left us a note.")
+seattle.notes << Note.create(content: "Did ritual and headed to Raven Court; spent time with the Darnician 5th Legion (lost in the North centuries ago as part of a failed colonization attempt. The General's name was Aula Venator.) Realized the took us off the path and we backtracked.")
+nyc.notes << Note.create(content: "Got to Raven Court, got picked on by invisible gnomes. Mattias put on Raven Helm, gave us feathers making us part of his party so we could see everything. We had dinner. Frederick forged and gained status (+5 forging). Slept the night. Merrit snuck into library to read like a nerd. Learned about Raven Court somewhat.")
+nyc.notes << Note.create(content: "Mattias dreamed of the duels. So Mattias dueled someone, and so did Merrit and Marcus (all 3 M's dueled, W/L/W) Mattias, Owyn, Marcus wagered on a duel and lost. (turned down wager for 10 minues with Lisheki because if we lost we'd have to steal Albino Ravens from the Queen's tower and we were not ready to commit to that yet). Current Status: Frederick: 5, Mattias: 5, Marcus: 2?, Owyn: 1, Merrit: 0")
+
+syracuse.notes << Note.create(content: "in thrall to a demon who allows me use of his magic", secret:1)
+syracuse.notes << Note.create(content: "I never actually sleep.", secret:1)
+michigan.notes << Note.create(content: "Am not really a half-elf", secret:1)
+texas.notes << Note.create(content: "Carries fake gold that can grow into a monster.", secret:1)
+texas.notes << Note.create(content: "The Raven Queen is attempting to control me, and I might be ok with that.", secret:1)
+texas.notes << Note.create(content: "Am the bastard son of the Baron", secret:1)
+nyc.notes << Note.create(content: "my sword is evil and has me possessed", secret:1)
+nyc.notes << Note.create(content: "a dream has bewitched me into seeking out the elven maiden who's amulet I hold", secret:1)
+
